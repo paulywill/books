8dc90c82685b741746329aadcdb294fa023a87ea
Paul Gamble <pgamble@gmail.com>
2019-03-04 16:03:10 -0500

    cleanup

--- newtest.js
+++
@@ -1,8 +0,0 @@
-async function getUserAsync(name) {
-    let response = await fetch(`https://api.github.com/users/${name}`);
-    let data = await response.json()
-    return data;
-}
-
-getUserAsync('paulywill')
-    .then(data => console.log(data));
--- scripts.js
+++ scripts.js
@@ -1,35 +1,89 @@
-/*
 
-document.getElementById(".search-book").addEventListener("click", bookSearch);
-var titleValue = document.querySelector('.title');
+const apiKey = 'AIzaSyB1MYJPUGXWOx13zt6fZY0zP4oiElQNezw'
 
-console.log('titleValue: ' + titleValue);
+function clearBox(elementID) {
+    document.getElementById(elementID).innerHTML = "";
+}
 
-const apiKey = "AIzaSyB1MYJPUGXWOx13zt6fZY0zP4oiElQNezw"
+
+function handleClick() {
+    clearBox("content");
+    document.querySelector('.search-book').addEventListener('click', getBook);
+}
 
 
 function convertHTML(str) {
-    newStr = str.replace(" ", "+");
+    newStr = str.replace(/ /g, "+");
     return newStr
 }
 
-function bookSearch(query) {
-    console.log("query: " + query);
-    strQry_head = "https://www.googleapis.com/books/v1/volumes?q=";
-    strQry_tail = "=full&key=AIzaSyB1MYJPUGXWOx13zt6fZY0zP4oiElQNezw";
-    console.log("convertHTML: " + convertHTML(query));
-    bQuery = "'" + strQry_head.concat(convertHTML(query), strQry_tail) + "'";
-    console.log("bQuery: " + bQuery);
 
-     fetch(bQuery)
-         .then(function (response) {
-             return response.json();
-         })
-         .then(function (myJson) {
-             console.log(JSON.stringify(myJson));
-         });
+function getBook() {
+
+    let titleHolder = document.getElementById("title").value;
+    console.log("titleHolder: " + titleHolder);
+    let title = convertHTML(titleHolder)
+    console.log("the title: " + title);
+    url = `https://www.googleapis.com/books/v1/volumes?q=${title}&projection=full&key=${apiKey}`
+    console.log("url: " + url);
+
+
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let items = data.items;
+            console.log(data.items); 
+            for (var i = 0; i < items.length; i++) {
+                
+                // Volume info
+                let item = items[i].volumeInfo;
+        
+                // Author
+                let author = item.authors;
+                console.log("item.authors: " + item.authors);
+    
+                // Image link
+                let imgLink = item.imageLinks.thumbnail;
+                console.log("item.imageLinks.thumbnail: " + item.imageLinks.thumbnail);
+
+                // Title
+                let title = item.title;
+                console.log("item.title: " + item.title);
+
+                // Publishing Company
+                let publisher = item.publisher;
+                console.log("item.publisher: " + item.publisher);
+
+                // Published Date
+                let publishedDate = item.publishedDate;
+                console.log("item.publisher: " + item.publishedDate);
+                
+                // Info Link  Date
+                let infoLink = item.infoLink;
+                console.log("item.publisher: " + item.infoLink);
+                
+
+
+
+                document.getElementById("content").innerHTML 
+                += '<div class="card"><br>' + 
+                '<img src=' + imgLink + '><br>' +
+                author + '<br>' + 
+                title + '<br>' +
+                publisher + '<br>' +
+                publishedDate + '<br>' +
+                '<a href='+ infoLink + '>More Info</a></div>';
+
+           
+            }    
+        
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    
+         
 
 }
-
-
-*/
